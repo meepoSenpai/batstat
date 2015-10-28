adfeab7a6c5e20a33d0d29a81e328b5e685f244e
Ian List <ian_alexander.list@mailbox.tu-dresden.de>
2015-10-28 13:19:22 +0100

    Fixed my minor major fuck up

--- batstat.py
+++ batstat.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 '''
-Small module that displays batter capacity of either 2 batteries
+Small module that displays battery capacity of either 2 batteries
 if your laptop has 2 of them or just of your one battery
 '''
 
@@ -9,7 +9,7 @@ try:
         BAT1_CAPACITY = bat1_cap.read()
     with open('/sys/class/power_supply/BAT0/capacity', 'r') as bat0_cap:
         BAT0_CAPACITY = bat0_cap.read()
-    print(int((BAT1_CAPACITY + BAT0_CAPACITY) / 2))
+    print(int((int(BAT1_CAPACITY) + int(BAT0_CAPACITY)) / 2))
 except IOError:
     try:
         with open('/sys/class/power_supply/BAT0/capacity', 'r') as bat0_cap:
